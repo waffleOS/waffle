8be8451143740cfd0b372683cb1a33d7630d6477
David Qu <davidqu12345@gmail.com>
2016-01-19 14:07:09 -0800

    Moved paddle code above c_start to use it.

--- src/booter/game.c
+++ src/booter/game.c
@@ -1,6 +1,30 @@
 #include "interrupts.h"
 #include "video.h"
 
+typedef struct Player {
+    int paddle_x;
+    int paddle_y;
+    int score;
+} Player;
+
+#define PADDLE_LENGTH 3
+
+movePaddle(int x, int y, Player *p) {
+    int i;
+
+    // Remove previous paddle position
+    for(i = 0; i < PADDLE_LENGTH; i++) {
+        clearPixel(p->paddle_x, p->paddle_y + i);
+    }
+
+    // Draw new paddle
+    for(i = 0; i < PADDLE_LENGTH; i++) {
+        setPixel(x, y + i, GREEN, (char) 186);
+    }
+
+    p->paddle_x = x;
+    p->paddle_y = y;
+}
 /* This is the entry-point for the game! */
 void c_start(void) {
     /* TODO:  You will need to initialize various subsystems here.  This
@@ -27,32 +51,11 @@ void c_start(void) {
      init_timer();
      init_keyboard();
      enable_interrupts();
-
+     
+     Player p1;
+     movePaddle(10, 20, &p1);
     /* Loop forever, so that we don't fall back into the bootloader code. */
     while (1) {}
 }
 
-typedef struct Player {
-    int paddle_x;
-    int paddle_y;
-    int score;
-} Player;
 
-#define PADDLE_LENGTH 3
-
-movePaddle(int x, int y, Player *p) {
-    int i;
-
-    // Remove previous paddle position
-    for(i = 0; i < PADDLE_LENGTH; i++) {
-        clearPixel(p->paddle_x, p->paddle_y + i);
-    }
-
-    // Draw new paddle
-    for(i = 0; i < PADDLE_LENGTH; i++) {
-        setPixel(x, y + i, GREEN, (char) 186);
-    }
-
-    p->paddle_x = x;
-    p->paddle_y = y;
-}
