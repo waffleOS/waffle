71a49ad59748d2a17707e52456b20b8acf9bf627
Rahul Bachal <rbachal@caltech.edu>
2016-01-18 16:12:55 -0800

    Bug in installing interrupt handlers for types

--- src/booter/interrupts.c
+++ src/booter/interrupts.c
@@ -226,8 +226,8 @@ void install_interrupt_handler(int num, void *handler) {
      *        INCORPORATE THE ABOVE COMMENTS IF YOU WISH.)
      */
 
-     interrupt_descriptor_table[num].offset_15_0 = (handler << 16) >> 16;
-     interrupt_descriptor_table[num].offset_31_16 = handler >> 16;
+     interrupt_descriptor_table[num].offset_15_0 = ((int) handler << 16) >> 16;
+     interrupt_descriptor_table[num].offset_31_16 = (int) handler >> 16;
      interrupt_descriptor_table[num].selector = SEL_CODESEG;
      interrupt_descriptor_table[num].type_attr = 0;
 }
