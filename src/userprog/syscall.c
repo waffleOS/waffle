2c0f66685359540a1afdb780c6febc7e9eb7e079
Rahul Bachal <rbachal@caltech.edu>
2016-02-07 00:03:57 -0800

    Basic structure of syscall_handler

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,7 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "devices/shutdown.h"
 
 static void syscall_handler(struct intr_frame *);
 
@@ -12,17 +13,58 @@ void syscall_init(void) {
 
 static void syscall_handler(struct intr_frame *f UNUSED) {
     printf("system call!\n");
+    int syscall_num = *f->esp;
+    switch (syscall_num)
+    {
+        case SYS_HALT:
+            halt();
+            break;
+        case SYS_EXIT:
+            exit(status);
+            break;
+        case SYS_EXEC:
+            exec();
+            break;
+        case SYS_WAIT:
+            wait();
+            break;
+        case SYS_CREATE:
+            create();
+            break;
+        case SYS_REMOVE:
+            remove();
+            break;
+        case SYS_FILESIZE:
+            filesize();
+            break;
+        case SYS_READ:
+            read();
+            break;
+        case SYS_WRITE:
+            write();
+            break;
+        case SYS_SEEK:
+            seek();
+            break;
+        case SYS_TELL:
+            tell();
+            break;
+        case SYS_CLOSE:
+            close();
+            break;
+    }
+
     thread_exit();
 }
 
 void halt(void)
-{
+
     shutdown_power_off();    
 }
 
 void exit(int status)
 {
-
+    thread_exit();
 }
 
 pid_t exec(const char * cmd_line)
