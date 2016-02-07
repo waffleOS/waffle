6ce7b98299865f497f71b629d126acee568bd0e6
Rahul Bachal <rbachal@caltech.edu>
2016-02-06 23:33:16 -0800

    System call functions

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -15,3 +15,67 @@ static void syscall_handler(struct intr_frame *f UNUSED) {
     thread_exit();
 }
 
+void halt(void)
+{
+    
+}
+
+void exit(int status)
+{
+
+}
+
+pid_t exec(const char * cmd_line)
+{
+
+}
+
+int wait(pid_t pid)
+{
+
+}
+
+bool create(const char * file, unsigned int initial_size)
+{
+
+}
+
+bool remove(const char * file)
+{
+
+}
+
+int open(const char * file)
+{
+
+}
+
+int filesize(int fd)
+{
+
+}
+
+int read(int fd, void * buffer, unsigned int size)
+{
+
+}
+
+int write(int fd, const void * buffer, unsigned int size)
+{
+
+}
+
+void seek(int fd, unsigned int position)
+{
+
+}
+
+unsigned int tell(int fd)
+{
+
+}
+
+void close (int fd)
+{
+
+}
