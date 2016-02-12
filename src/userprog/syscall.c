540d20385bcee0adf0ffb5ce9cf8a8fac4c0105d
David Qu <davidqu12345@gmail.com>
2016-02-11 16:14:57 -0800

    Renamed kernel syscall functions to start with do_

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,7 +6,21 @@
 #include "devices/shutdown.h"
 #include "lib/user/syscall.h"
 
+/* Module specific function prototypes. */
 static void syscall_handler(struct intr_frame *);
+void do_halt(void);
+void do_exit(int status);
+pid_t do_exec(const char * cmd_line);
+int do_wait(pid_t pid);
+bool do_create(const char * file, unsigned int initial_size);
+bool do_remove(const char * file);
+int do_open(const char * file);
+int do_filesize(int fd);
+int do_read(int fd, void * buffer, unsigned int size);
+int do_write(int fd, const void * buffer, unsigned int size);
+void do_seek(int fd, unsigned int position);
+unsigned int do_tell(int fd);
+void do_close(int fd);
 
 void syscall_init(void) {
     intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
@@ -33,64 +47,65 @@ static void syscall_handler(struct intr_frame *f UNUSED) {
     switch (syscall_num)
     {
         case SYS_HALT:
-            halt();
+            do_halt();
             break;
         case SYS_EXIT:
             status = *((int *) f->esp + 4);
-            exit(status);
+            do_exit(status);
             break;
         case SYS_EXEC:
             cmd_line = *((const char **) f->esp + 4);
-            pid = exec(cmd_line);
+            pid = do_exec(cmd_line);
             *((pid_t *) f->eax) = pid;
            break;
         case SYS_WAIT:
             pid = *((pid_t *) f->esp + 4);
-            status = wait(pid);
+            status = do_wait(pid);
             *((int *) f->eax) = status;
             break;
         case SYS_CREATE:
             file = *((const char **) f->esp + 4);
             initial_size = *((int *) f->esp + 8);
-            success = create(file, initial_size);
+            success = do_create(file, initial_size);
             *((bool *) f->eax) = success;
             break;
         case SYS_REMOVE:
             file = *((const char **) f->esp + 4);
+            success = do_remove(file);
             *((bool *) f->eax) = success;
             break;
         case SYS_FILESIZE:
             fd = *((int *) f->esp + 4);
-            size = filesize(fd);
+            size = do_filesize(fd);
             *((int *) f->eax) = size;
             break;
         case SYS_READ:
             fd = *((int *) f->esp + 4);
             buffer = *(f->esp + 8);
             size = *((unsigned int *) f->esp + 12);
-            num_bytes = read(fd, buffer, size);
+            num_bytes = do_read(fd, buffer, size);
             *((int *) f->eax) = num_bytes;
             break;
         case SYS_WRITE:
             fd = *((int *) f->esp + 4);
             buffer = *(f->esp + 8);
             size = *((unsigned int *) f->esp + 12);
-            num_bytes = write(fd, buffer, size);
+            num_bytes = do_write(fd, buffer, size);
             *((int *) f->eax) = num_bytes;
             break;
         case SYS_SEEK:
             fd = *((int *) f->esp + 4);
             position = *((unsigned int *) f->esp + 8);
-            seek(fd, position);
+            do_seek(fd, position);
             break;
         case SYS_TELL:
             fd = *((int *) f->esp + 4);
-            position = tell(fd);
+            position = do_tell(fd);
             *((unsigned int *) f->eax) = position;
             break;
         case SYS_CLOSE:
             fd = *((int *) f->esp + 4);
-            close(fd);
+            do_close(fd);
             break;
     }
 
@@ -112,52 +127,52 @@ pid_t exec(const char * cmd_line)
 
 }
 
-int wait(pid_t pid)
+int do_wait(pid_t pid)
 {
 
 }
 
-bool create(const char * file, unsigned int initial_size)
+bool do_create(const char * file, unsigned int initial_size)
 {
 
 }
 
-bool remove(const char * file)
+bool do_remove(const char * file)
 {
 
 }
 
-int open(const char * file)
+int do_open(const char * file)
 {
 
 }
 
-int filesize(int fd)
+int do_filesize(int fd)
 {
 
 }
 
-int read(int fd, void * buffer, unsigned int size)
+int do_read(int fd, void * buffer, unsigned int size)
 {
 
 }
 
-int write(int fd, const void * buffer, unsigned int size)
+int do_write(int fd, const void * buffer, unsigned int size)
 {
 
 }
 
-void seek(int fd, unsigned int position)
+void do_seek(int fd, unsigned int position)
 {
 
 }
 
-unsigned int tell(int fd)
+unsigned int do_tell(int fd)
 {
 
 }
 
-void close (int fd)
+void do_close (int fd)
 {
 
 }
