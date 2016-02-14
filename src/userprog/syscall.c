4d05f1a969f50bec67891ca5cc42bd7817f7f338
RobertEng <googlypantsroberteng@gmail.com>
2016-02-14 15:59:32 -0800

    fixed up validator, implemented (maybe?) some of the syscalls

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -112,7 +112,7 @@ static void syscall_handler(struct intr_frame *f UNUSED) {
             break;
     }
 
-    thread_exit();
+/*    thread_exit();*/
 }
 
 void do_halt(void)
@@ -129,12 +129,21 @@ void do_exit(int status)
 
 pid_t do_exec(const char * cmd_line)
 {
+    /* For ease, let's say process ids and thread ids line up in a one-to-one
+    mapping */
+    pid_t child = process_execute(cmd_line);
 
+    /* TODO: Implement synchronization */
+
+    if(child == TID_ERROR) {
+        return -1;
+    }
+    return child;
 }
 
 int do_wait(pid_t pid)
 {
-
+    process_wait(pid);
 }
 
 bool do_create(const char * file, unsigned int initial_size)
@@ -183,18 +192,15 @@ void do_close (int fd)
 }
 
 bool validate_pointer(void *ptr) {
-    /* Check if pointer is in correct space. */
-    if (ptr == NULL || !is_user_vaddr(ptr)) {
-        return false;
-    }
-    
     struct thread *cur = thread_current();
     uint32_t *pd = cur->pagedir;
 
-    /* Check if in page directory of the current thread. */
-    if (pagedir_get_page(pd, ptr) == NULL) {
+    /* Check if pointer is in correct space. Check if in page directory of the
+    current thread. */
+    if (ptr == NULL || !is_user_vaddr(ptr) || pagedir_get_page(pd, ptr) == NULL) {
+        process_exit();
         return false;
     }
-
+    
     return true;
 }
